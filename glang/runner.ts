9a3b2d3b88b5f6539f853294635ebe530a1feee2
Leonardone <neetsdkasu@gmail.com>
2026-06-13 12:50:38 +0900

    update

---
+++ glang/runner.ts
@@ -0,0 +1,511 @@
+//
+// Runner
+// 
+import Logger, { LogLevel } from "logger";
+const log = new Logger("runner", LogLevel.ALL);
+
+import { Cmd, Program, StdFunc, Source } from "command";
+import { Result } from "utils";
+import * as U from "utils";
+
+export class RuntimeError 
+{
+    readonly msg: string;
+    readonly src: Source | undefined;
+
+    constructor(msg: string, src: Source | undefined) {
+        this.msg = msg;
+        this.src = src;
+    }
+
+    toString(): string {
+        return `RuntimeError{ msg: ${this.msg}, src: ${this.src} }`;
+    }
+}
+export type IsRunning = boolean;
+export type RunnerResult = Result<IsRunning,RuntimeError>;
+
+const RUNNING: RunnerResult = Result.ok(true);
+const ENDED: RunnerResult = Result.ok(false);
+
+export interface IO {
+    stderr(s: string): void;
+}
+
+type ValueType = boolean | number | string;
+type VarType = boolean | number | string | boolean[] | boolean[][] | boolean[][][] | number[] | number[][] | number[][][] | string[] | string[][] | string[][][];
+
+export class Runner {
+    readonly #program: Readonly<number[]>;
+    readonly #litStrPool: Readonly<string[]>;
+    readonly #io: IO;
+    readonly #sourceMap: Readonly<Source[]>;
+
+    #pos: number = 0;
+    #isRunning: boolean = true;
+    #error: U.Option<RunnerResult> = U.Option.none();
+
+    #block: VarType[][];
+    #blockStack: VarType[][][];
+
+    #valueStack: ValueType[] = [];
+    #addressStack: number[] = [];
+
+    constructor(program: Program, io: IO) {
+        this.#program = program.program;
+        this.#litStrPool = program.litStrPool;
+        this.#sourceMap = program.sourceMap;
+        this.#block = new Array(program.totalBlockCount).fill([]).map( () => [] );
+        this.#blockStack = new Array(program.totalBlockCount).fill([]).map( () => [] );
+        this.#io = io;
+    }
+
+    #findSource(addr: number): Source | undefined {
+        const index = U.binarySearch(this.#sourceMap, (s => s.addr.min >= addr ));
+        if (index !== undefined && this.#sourceMap[index].addr.include(addr)) {
+            return this.#sourceMap[index];
+        }
+        return undefined;
+    }    
+
+    #runtimeError(addr: number, msg: string): RunnerResult {
+        const src = this.#findSource(addr);
+        const err: RunnerResult = Result.err(new RuntimeError(msg, src));
+        this.#error = U.Option.some(err);
+        this.#isRunning = false;
+        return err;
+    }
+
+    step(): RunnerResult {
+        if (!this.#isRunning) {
+            return this.#error.getOr(ENDED);
+        }
+        const cmd: Cmd = this.#program[this.#pos++];
+        log.dump("pos", this.#pos);
+        log.dump("cmd", Cmd[cmd]);
+        switch (cmd) {
+            case Cmd.NOP:
+                throw new Error("bug");
+            case Cmd.END: 
+                {
+                    this.#isRunning = false;
+                }
+                return ENDED;
+            case Cmd.POP:
+                {
+                    this.#valueStack.pop();
+                }
+                break;
+            case Cmd.DUP:
+                {
+                    this.#valueStack.push(this.#valueStack.at(-1)!);
+                }
+                break;
+            case Cmd.DUPN:
+                {
+                    const N: number = this.#program[this.#pos++];
+                    for (let i = 0; i < N; i++) {
+                        this.#valueStack.push(this.#valueStack.at(-N)!);
+                    }
+                }
+                break;
+            case Cmd.SWAP:
+                {
+                    const oldTop = this.#valueStack.pop()!;
+                    const newTop = this.#valueStack.pop()!;
+                    this.#valueStack.push(oldTop);
+                    this.#valueStack.push(newTop);
+                }
+                break;
+            case Cmd.BPUSH_TRUE:
+                {
+                    this.#valueStack.push(true);
+                }
+                break;
+            case Cmd.BPUSH_FALSE:
+                {
+                    this.#valueStack.push(false);
+                }
+                break;
+            case Cmd.BNOT:
+            case Cmd.BAND:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.BOR:
+                {
+                    const right = this.#valueStack.pop() as boolean;
+                    const left = this.#valueStack.pop() as boolean;
+                    this.#valueStack.push(left || right);
+                }
+                break;
+            case Cmd.BEQ:
+            case Cmd.BNE:
+            case Cmd.GET_BVAR:
+            case Cmd.SET_BVAR:
+            case Cmd.GET_BARR1D:
+            case Cmd.SET_BARR1D:
+            case Cmd.GET_BARR2D:
+            case Cmd.SET_BARR2D:
+            case Cmd.GET_BARR3D:
+            case Cmd.SET_BARR3D:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.FPUSH:
+                {
+                    const floatValue = this.#program[this.#pos++];
+                    this.#valueStack.push(floatValue);
+                }
+                break;
+            case Cmd.FADD:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push(left + right);
+                }
+                break;
+            case Cmd.FSUB:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push(left - right);
+                }
+                break;
+            case Cmd.FMUL:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push(left * right);
+                }
+                break;
+            case Cmd.FDIV:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    const value = left / right;
+                    if (U.isInfinityOrNaN(value)) {
+                        return this.#runtimeError(this.#pos-1, `wrong divide: ${left} / ${right}`);
+                    }
+                    this.#valueStack.push(value);
+                }
+                break;
+            case Cmd.FNEGA:
+            case Cmd.FEQ:
+            case Cmd.FNE:
+            case Cmd.FLT:
+            case Cmd.FLE:
+            case Cmd.FGT:
+            case Cmd.FGE:
+            case Cmd.GET_FVAR:
+            case Cmd.SET_FVAR:
+            case Cmd.GET_FARR1D:
+            case Cmd.SET_FARR1D:
+            case Cmd.GET_FARR2D:
+            case Cmd.SET_FARR2D:
+            case Cmd.GET_FARR3D:
+            case Cmd.SET_FARR3D:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.IPUSH:
+                {
+                    const intValue = this.#program[this.#pos++];
+                    this.#valueStack.push(intValue);
+                }
+                break;
+            case Cmd.IADD:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push((left + right) & 0xFFFFFFFF);
+                }
+                break;
+            case Cmd.ISUB:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push((left - right) & 0xFFFFFFFF);
+                }
+                break;
+            case Cmd.IMUL:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push(Math.imul(left, right));
+                }
+                break;
+            case Cmd.IDIV:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    const value = left / right;
+                    if (U.isInfinityOrNaN(value)) {
+                        return this.#runtimeError(this.#pos-1, `wrong divide: ${left} / ${right}`);
+                    }
+                    this.#valueStack.push(Math.trunc(value));
+                }
+                break;
+            case Cmd.IREM:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    const value = left % right;
+                    if (U.isInfinityOrNaN(value)) {
+                        return this.#runtimeError(this.#pos-1, `wrong divide: ${left} % ${right}`);
+                    }
+                    this.#valueStack.push(value);
+                }
+                break;
+            case Cmd.INEGA:
+            case Cmd.IASHIFTL:
+            case Cmd.IASHIFTR:
+            case Cmd.ILSHIFTL:
+            case Cmd.ILSHIFTR:
+            case Cmd.INOT:
+            case Cmd.IAND:
+            case Cmd.IOR:
+            case Cmd.IXOR:
+            case Cmd.IEQ:
+            case Cmd.INE:
+            case Cmd.ILT:
+            case Cmd.ILE:
+            case Cmd.IGT:
+            case Cmd.IGE:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.GET_IVAR:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarId = this.#program[this.#pos++];
+                    const value = this.#block[blockId][blockVarId] as number;
+                    this.#valueStack.push(value);
+                }
+                break;                
+            case Cmd.SET_IVAR:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarId = this.#program[this.#pos++];
+                    const value = this.#valueStack.pop()!;
+                    this.#block[blockId][blockVarId] = value;
+                }
+                break;
+            case Cmd.GET_IARR1D:
+            case Cmd.SET_IARR1D:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.GET_IARR2D:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarId = this.#program[this.#pos++];
+                    const index2 = this.#valueStack.pop() as number;
+                    const index1 = this.#valueStack.pop() as number;
+                    const arr = this.#block[blockId][blockVarId] as number[][];
+                    const value = arr.at(index1)?.at(index2);
+                    if (value === undefined) {
+                        return this.#runtimeError(this.#pos-3, `index out of bound: [${index1}][${index2}]`);
+                    }
+                    this.#valueStack.push(value);
+                }
+                break;
+            case Cmd.SET_IARR2D:
+            case Cmd.GET_IARR3D:
+            case Cmd.SET_IARR3D:
+            case Cmd.SPUSH:
+            case Cmd.SCONCAT:
+            case Cmd.SEQ:
+            case Cmd.SNE:
+            case Cmd.SLT:
+            case Cmd.SLE:
+            case Cmd.SGT:
+            case Cmd.SGE:
+            case Cmd.GET_SVAR:
+            case Cmd.SET_SVAR:
+            case Cmd.GET_SARR1D:
+            case Cmd.SET_SARR1D:
+            case Cmd.GET_SARR2D:
+            case Cmd.SET_SARR2D:
+            case Cmd.GET_SARR3D:
+            case Cmd.SET_SARR3D:
+            case Cmd.APUSH_BARR1D:
+            case Cmd.APUSH_BARR2D:
+            case Cmd.APUSH_BARR3D:
+            case Cmd.APUSH_FARR1D:
+            case Cmd.APUSH_FARR2D:
+            case Cmd.APUSH_FARR3D:
+            case Cmd.APUSH_IARR1D:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.APUSH_IARR2D:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarId = this.#program[this.#pos++];
+                    this.#valueStack.push(blockId, blockVarId);
+                }
+                break;
+            case Cmd.APUSH_IARR3D:
+            case Cmd.APUSH_SARR1D:
+            case Cmd.APUSH_SARR2D:
+            case Cmd.APUSH_SARR3D:
+            case Cmd.INIT_BARR1D:
+            case Cmd.INIT_BARR2D:
+            case Cmd.INIT_BARR3D:
+            case Cmd.INIT_FARR1D:
+            case Cmd.INIT_FARR2D:
+            case Cmd.INIT_FARR3D:
+            case Cmd.INIT_IARR1D:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.INIT_IARR2D:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarId = this.#program[this.#pos++];
+                    const size1 = this.#program[this.#pos++];
+                    const size2 = this.#program[this.#pos++];
+                    this.#block[blockId][blockVarId] = new Array<number[]>(size1).fill([]).map( () => new Array<number>(size2).fill(0) );
+                }
+                break;
+            case Cmd.INIT_IARR3D:
+            case Cmd.INIT_SARR1D:
+            case Cmd.INIT_SARR2D:
+            case Cmd.INIT_SARR3D:
+            case Cmd.JUMP:
+            case Cmd.JUMP_IF_TRUE:
+            case Cmd.JUMP_IF_FALSE:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.CALL_STDFUNC:
+                {
+                    const stdfuncId = this.#program[this.#pos++] as StdFunc;
+                    return this.#callStdfunc(stdfuncId);
+                }
+            case Cmd.CALL_USERFUNC:
+                {
+                    const userfuncAddress = this.#program[this.#pos++];
+                    const returnAddress = this.#program[this.#pos++];
+                    this.#pos = userfuncAddress;
+                    this.#addressStack.push(returnAddress);
+                }
+                break;
+            case Cmd.RET:
+                throw new U.Unimplemented(Cmd[cmd]);
+            case Cmd.PUSH_BLOCK:
+                {
+                    const blockId = this.#program[this.#pos++];
+                    const blockVarCount = this.#program[this.#pos++];
+                    this.#blockStack[blockId].push(this.#block[blockId]);
+                    this.#block[blockId] = new Array(blockVarCount).fill(undefined);
+                }
+                break;
+            case Cmd.POP_BLOCK:
+            case Cmd.PRINT:
+            default:
+                throw new U.Unimplemented(Cmd[cmd]);
+        }
+        return RUNNING;
+    }
+
+    #callStdfunc(stdfuncId: StdFunc): RunnerResult {
+        switch (stdfuncId) {
+            case StdFunc.CBOOL_FROM_BOOLEAN:
+                // ok
+                break;
+            case StdFunc.CBOOL_FROM_FLOAT:
+            case StdFunc.CBOOL_FROM_INTEGER:
+            case StdFunc.CBOOL_FROM_STRING:
+            case StdFunc.CFLOAT_FROM_BOOLEAN:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.CFLOAT_FROM_FLOAT:
+                // ok
+                break;
+            case StdFunc.CFLOAT_FROM_INTEGER:
+                // ok
+                break;
+            case StdFunc.CFLOAT_FROM_STRING:
+            case StdFunc.CINT_FROM_BOOLEAN:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.CINT_FROM_FLOAT:
+                // ok
+                break;
+            case StdFunc.CINT_FROM_INTEGER:
+                // ok
+                break;
+            case StdFunc.CINT_FROM_STRING:
+            case StdFunc.CSTR_FROM_BOOLEAN:
+            case StdFunc.CSTR_FROM_FLOAT:
+            case StdFunc.CSTR_FROM_INTEGER:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.CSTR_FROM_STRING:
+                // ok
+                break;
+            case StdFunc.SIN:
+            case StdFunc.COS:
+            case StdFunc.TAN:
+            case StdFunc.ABS_FLOAT:
+            case StdFunc.ABS_INTGER:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.SIGN_FLOAT:
+                {
+                    const value = this.#valueStack.pop() as number;
+                    this.#valueStack.push(Math.sign(value));
+                }
+                break;
+            case StdFunc.SIGN_INTEGER:
+                {
+                    const value = this.#valueStack.pop() as number;
+                    this.#valueStack.push(Math.sign(value));
+                }
+                break;
+            case StdFunc.MIN_FLOAT:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.MIN_INTEGER:
+                {
+                    const right = this.#valueStack.pop() as number;
+                    const left = this.#valueStack.pop() as number;
+                    this.#valueStack.push(Math.min(left, right));
+                }
+                break;
+            case StdFunc.MAX_FLOAT:
+            case StdFunc.MAX_INTEGER:
+            case StdFunc.POW:
+            case StdFunc.SQRT:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.FLOOR:
+                {
+                    const value = this.#valueStack.pop() as number;
+                    this.#valueStack.push(Math.floor(value));
+                }
+                break;
+            case StdFunc.CEIL:
+            case StdFunc.SIZE_BARR1D:
+            case StdFunc.SIZE_BARR2D:
+            case StdFunc.SIZE_BARR3D:
+            case StdFunc.SIZE_FARR1D:
+            case StdFunc.SIZE_FARR2D:
+            case StdFunc.SIZE_FARR3D:
+            case StdFunc.SIZE_IARR1D:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+            case StdFunc.SIZE_IARR2D:
+                {
+                    const dim = this.#valueStack.pop() as number;
+                    const blockVarId = this.#valueStack.pop() as number;
+                    const blockId = this.#valueStack.pop() as number;
+                    const arr = this.#block[blockId][blockVarId] as number[][];
+                    switch (dim) {
+                        case 1:
+                            this.#valueStack.push(arr.length);
+                            break;
+                        case 2:
+                            this.#valueStack.push(arr[0].length);
+                            break;
+                        default:
+                            return this.#runtimeError(this.#pos-3, `wrong argument stdfunc size(${dim})`);
+                    }
+                }
+                break;
+            case StdFunc.SIZE_IARR3D:
+            case StdFunc.SIZE_SARR1D:
+            case StdFunc.SIZE_SARR2D:
+            case StdFunc.SIZE_SARR3D:
+            case StdFunc.SEL_BOOLEAN:
+            case StdFunc.SEL_FLOAT:
+            case StdFunc.SEL_INTEGER:
+            case StdFunc.SEL_STRING:
+            default:
+                throw new U.Unimplemented(StdFunc[stdfuncId]);
+        }
+
+        return RUNNING;
+    }
+}
+
+export default Runner;
